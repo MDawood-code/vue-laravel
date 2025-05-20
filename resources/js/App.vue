ff5dcc74ac8880f380c6cbaa4a317360d363f779
MDawood-code <mdawood0332@gmail.com>
2025-05-20 14:06:42 +0500

    first comment

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+        <Posts />
+    </div>
+</template>
+<script>
+import Posts from "./components/Posts.vue";
+export default {
+    components: {
+        Posts,
+    },
+};
+</script>
